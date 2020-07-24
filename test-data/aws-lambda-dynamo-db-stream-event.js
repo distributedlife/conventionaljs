76157b9f88f4a1a42bfba3aa599ebb324079f632
Ryan Boucher <ryan.boucher@distributedlife.com>
2020-07-24 10:55:07 +1000

    Failing test for exported function

---
+++ test-data/aws-lambda-dynamo-db-stream-event.js
@@ -0,0 +1,8 @@
+/**
+ * @param {AWSLambda.DynamoDBStreamEvent} event 
+ * @param {AWSLambda.Context} context 
+ * @param {AWSLambda.Callback} callback 
+ */
+exports.handler = (event, context, callback) => {
+  callback(null, null)
+};
